af0f32f98d43a1c9eea778af81e2cd8b25e538eb
J-Haze <59891689+J-Haze@users.noreply.github.com>
2020-10-02 19:51:22 -0700

    Saving.

---
+++ .history/src/Components/Beach_20201002194754.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "../App.css";
+import beachPhoto from "../assets/beach.jpg";
+
+function Beach(props) {
+
+
+
+
+
+    return (
+        <div>
+            <div id="beach-container" className="photo-container">
+                <img id="beach-photo" className="photo" src={beachPhoto} />
+                {(props.isWaldoFound) ?
+                    <div className="waldo-beach found"></div>
+                    :
+                    <div className="waldo-beach" onClick={props.foundWaldo}> </div> 
+                }               
+            </div>
+
+        </div>
+    )
+}
+
+export default Beach;
+
